58c2bd4cad86d60c825984c37d42c254565c6c4b
mahash0501 <mahash0501@gmail.com>
2026-09-16 14:44:55 +0000

    Initial 3-tier application

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psycopg2
+
+app = Flask(__name__)
+CORS(app)
+
+def get_db_connection():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="devopsdb",
+        user="postgres",
+        password=os.getenv("DB_PASSWORD")
+    )
+    return connection
+
+
+@app.route("/")
+def home():
+    return "Hello from 3-Tier DevOps Project!"
+
+
+@app.route("/health")
+def health():
+    return "Healthy"
+
+
+@app.route("/api/users")
+def users():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT id, name, role FROM users;")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    users_list = []
+
+    for row in rows:
+        users_list.append({
+            "id": row[0],
+            "name": row[1],
+            "role": row[2]
+        })
+
+    return jsonify(users_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+fetch("http://localhost:5000/api/users")
+    .then(response => response.json())
+    .then(users => {
+        const table = document.getElementById("users");
+
+        users.forEach(user => {
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.role}</td>
+            `;
+
+            table.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
